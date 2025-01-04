e2b4a08afdc53fdc5dda177f8a2a2f77e42c260d
Dwiyan Rezkia Putra <dwiyanrp@gmail.com>
2025-01-04 19:11:14 +0700

    add nvim plugin debugloop/telescope-undo

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,3 +7,4 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<C-f>", "<cmd>!tmux neww ~/sesh-connect.sh<CR>", { silent = true })
+map("n", "<Space>u", ":Telescope undo<CR>")
