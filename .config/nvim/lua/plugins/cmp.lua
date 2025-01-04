c01d761850cd6e5c16bfcfd24342eb18f7c5dab5
Dwiyan Rezkia Putra <dwiyanrp@gmail.com>
2025-01-04 18:22:43 +0700

    improve supermaven with changing it accept_suggestion default keymaps

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -4,7 +4,22 @@ return {
     dependencies = {
       {
         "supermaven-inc/supermaven-nvim",
-        opts = {},
+        config = function()
+          require("supermaven-nvim").setup {
+            keymaps = {
+              accept_suggestion = "<C-k>",
+              clear_suggestion = "<C-]>",
+              accept_word = "<C-j>",
+            },
+            ignore_filetypes = {},
+            disable_inline_completion = false,
+            disable_keymaps = false,
+            condition = function()
+              return false
+            end,
+            log_level = "info",
+          }
+        end,
       },
     },
   },
