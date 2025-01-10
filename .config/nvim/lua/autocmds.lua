70a7a6c648e29f90c0cd1df65b6f68d77f77f7a1
Dwiyan Rezkia Putra <dwiyanrp@gmail.com>
2025-01-10 11:38:07 +0700

    fix autocmd automatically change directory

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -35,16 +35,8 @@ autocmd("VimEnter", {
 
     -- Check if no arguments were given or if a directory was specified.
     if #vim.fn.argv() == 0 or is_valid_dir(vim.fn.argv()[1]) then
-      require("nvim-tree.api").tree.open()
-    end
-  end,
-})
-
--- automatically change directory to the file's parent when opening a file
-autocmd("BufEnter", {
-  callback = function()
-    if vim.api.nvim_get_option_value("buftype", {}) ~= "terminal" then
       vim.cmd "lcd %:p:h"
+      require("nvim-tree.api").tree.open()
     end
   end,
 })
