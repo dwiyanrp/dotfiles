74150ada93eb9889211f935c3c97e9f7210bbf89
Dwiyan Rezkia Putra <dwiyanrp@gmail.com>
2025-01-11 12:44:13 +0700

    improve setup autocmds, keymaps & options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,40 +1,68 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
 
-vim.g.have_nerd_font = false
+-------------------------------------- options ------------------------------------------
+g.mapleader = ' '
+g.maplocalleader = ' '
+g.have_nerd_font = true
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+o.laststatus = 3
+o.showmode = false
 
-vim.opt.mouse = 'a'
+o.clipboard = 'unnamedplus'
+o.cursorline = true
+o.cursorlineopt = 'number'
 
-vim.opt.showmode = false
+-- Indenting
+o.expandtab = true
+o.shiftwidth = 2
+o.smartindent = true
+o.tabstop = 2
+o.softtabstop = 2
 
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+opt.fillchars = { eob = ' ' }
+o.ignorecase = true
+o.smartcase = true
+o.mouse = 'a'
 
+-- Numbers
+o.number = true
+o.numberwidth = 2
+o.ruler = false
+o.relativenumber = true
+
+-- disable nvim intro
+opt.shortmess:append 'sI'
+
+o.signcolumn = 'yes'
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 300
+o.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+o.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append '<>[]hl'
+
+-- disable some default providers
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has 'win32' ~= 0
+local sep = is_windows and '\\' or '/'
+local delim = is_windows and ';' or ':'
+vim.env.PATH = table.concat({ vim.fn.stdpath 'data', 'mason', 'bin' }, sep) .. delim .. vim.env.PATH
+
+-- remaining options from kickstart
 vim.opt.breakindent = true
-
-vim.opt.undofile = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = 'yes'
-
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 vim.opt.inccommand = 'split'
-
-vim.opt.cursorline = true
-
 vim.opt.scrolloff = 10
