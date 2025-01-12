dd308c076666a72cc17f94dc6733fd87e11a456c
Dwiyan Rezkia Putra <dwiyanrp@gmail.com>
2025-01-12 09:42:37 +0700

    improve conform

--- .config/nvim/lua/plugins/list.lua
+++ .config/nvim/lua/plugins/list.lua
@@ -399,22 +399,9 @@ local plugins = {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
@@ -429,11 +416,7 @@ local plugins = {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { 'gofmt', 'gofumpt', 'goimports', 'goimports-reviser', 'golines' },
       },
     },
   },
